ff4c6c6607bbb3326e0ef9e5fa9a154c5d2a0430
Shahmayousuf <shahmayousuf@gmail.com>
2024-02-02 22:09:38 +0530

    create auth.controller

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from 'mongoose'
-
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 const connectdb = async () => {
   try {
@@ -12,9 +13,13 @@ const connectdb = async () => {
   }
 };
 connectdb();
+
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 }
 );
+app.use("/api/user",userRouter);
+app.use('/api/auth',authRouter);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -12,7 +12,7 @@ const userSchema = mongoose.Schema(
       required: true,
       unique: true,
     },
-    email: {
+    password: {
       type: String,
       required: true,
     },
