2b44d6fbc079b2180382eca9f029e8807cef3e78
Shahmayousuf <shahmayousuf@gmail.com>
2024-02-02 09:32:37 +0530

    connect databse

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from 'mongoose'
+
+
+const connectdb = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/mern-estate");
+    console.log("mongodb connectd");
+  } catch (error) {
+    console.log("mongodb not connectd");
+    throw error;
+  }
+};
+connectdb();
 const app = express();
 
 app.listen(3000, () => {
