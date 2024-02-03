db39ec61e1562f5d4ef4eb403c4a0b2d3dc56e86
Shahmayousuf <shahmayousuf@gmail.com>
2024-02-03 21:55:24 +0530

    create middleware

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
-import mongoose from 'mongoose'
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const connectdb = async () => {
   try {
@@ -19,7 +19,17 @@ app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
-}
-);
-app.use("/api/user",userRouter);
-app.use('/api/auth',authRouter);
+});
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message =err.message || 'internal Server Error';
+  return res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message
+  })
+
+});
