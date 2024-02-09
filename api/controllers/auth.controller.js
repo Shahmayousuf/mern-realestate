aeae5fef8a518b555a4718cacd446fbf41bfa51d
Shahmayousuf <shahmayousuf@gmail.com>
2024-02-09 15:21:32 +0530

    started sign in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,27 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-export const signup = async (req, res,next) => {
+import { errorHandler } from "../utils/error.js";
+import jwt from  'jsonwebtoken'
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-  res.status(201).json("User created successfuly");
+    res.status(201).json("User created successfuly");
   } catch (error) {
-    next(error)
+    next(error);
   }
-  
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentials!"));
+    
+  } catch (error) {
+    next(error);
+  }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,7 @@
-import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
-  
+import express from "express";
+import { signup } from "../controllers/auth.controller.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.post("/signup",signup)
-export default router
+router.post("/signup", signup);
+export default router;
