99a9ebded20869b0595071a9b840a3386de73c51
Shahmayousuf <shahmayousuf@gmail.com>
2024-02-09 07:41:19 +0530

    complted signup funcationality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,35 +1,42 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 const SignUp = () => {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
-  const[loading, setLoading] = useState(false);
-
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true)
-    const res = await fetch("/api/auth/signup", {
-      method: "POST",
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
 
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    if(data.message===false){
-      setError(data.message);
-      setLoading(true)
-      return;
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/signin");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
     }
-    setLoading(false)
-    
-    console.log(data);
   };
-  console.log(formData);
+
   return (
     <div className="p-5 max-w-lg mx-auto ">
       <h1 className="text-3xl text-center font-bold my-7">Sign Up</h1>
@@ -56,10 +63,11 @@ const SignUp = () => {
           onChange={handleChange}
         />
         <button
+          disabled={loading}
           type="submit"
           className="bg-slate-700 rounded-lg text-white p-3 uppercase hover:opacity-90 disabled:opacity-80 "
         >
-          Sign Up
+          {loading ? "Loading..." : "SignUp"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -68,6 +76,7 @@ const SignUp = () => {
           <span className="text-blue-700 "> Sign In</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 };
