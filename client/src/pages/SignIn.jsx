9f2516554b053ad9d5ab8fe38771a5ed9c59df9e
Shahmayousuf <shahmayousuf@gmail.com>
2024-02-14 22:00:17 +0530

    add userSlice

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userSlice";
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      // setLoading(true);
+      dispatch(signInStart);
       const res = await fetch("/api/auth/signin", {
         method: "POST",
 
@@ -23,17 +31,20 @@ const SignIn = () => {
       });
       const data = await res.json();
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        // setLoading(false);
+        dispatch(signInFailure(data.message));
+        // setError(data.message);
 
         return;
       }
-      setLoading(false);
-      setError(null);
+      // setLoading(false);
+      dispatch(signInSuccess(data));
+      // setError(null);
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      // setLoading(false);
+      // setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
 
@@ -41,7 +52,6 @@ const SignIn = () => {
     <div className="p-5 max-w-lg mx-auto ">
       <h1 className="text-3xl text-center font-bold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col  gap-4">
-       
         <input
           type="email"
           placeholder="email"
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,16 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 
-    const initialstate={
+    const initialState={
         currentUser:null,
         error:null,
         loading:false,
     };
 export const userSlice=createSlice({
-     name:'User',
-     initialstate,
+     name:'user',
+     initialState,
      reducers:{
-        
+        signInStart:(state)=>{
+            state.loading=true
+        },
+        signInSuccess:(state,action)=>{
+            state.currentUser=action.payload;
+            state.loading=false;
+            state.error=null;
+        },
+        signInFailure:(state,action)=>{
+            state.error=action.payload;
+            state.loading=false;
+          
+        }
      }
      
 })
+export const{signInStart,signInSuccess,signInFailure}=userSlice.actions;
+export default userSlice.reducer
